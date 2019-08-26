9d56d65b16ee3c02a8d778415a4bb83784c2f8d5
iamtashi <tacmoktan@gmail.com>
2019-08-26 23:23:25 +0545

    Add styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,58 @@
 import React from 'react';
 
 //styles
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = (theme) => ({
 
-});
+//style variables
+let paddingVar= 20;
+const useNavStyles = makeStyles(theme => ({
+    headerClass: {
+        display: "flex",
+        justifyContent:"flex-end",
+        alignItems:"center",
+        height:57,
+        position:"fixed",
+        width:"100%",/* 
+        boxShadow: "0 1px 8px 0px" */
+    },
+    logoClass:{
+        margin:"0 auto 0 20px"
+    },
+    navbar:{
+        '& a':{
+            padding:paddingVar,
+            textDecoration:"none",
+            color:"black",
+            '&:hover':{
+                color:"white"
+            },
+            '&:nth-child(4)':{
+                marginRight:"20px"
+            }
+        }
+    }
+}));
 
-const Navbar = () => (
-    <>
-    <header id="header">
-        <div id="logo">
-            Tashi Lama
-        </div>
-        <nav>
-            <a href="#about">About</a>
-            <a href="#projects">Projects</a>
-            <a href="#skills">Skills</a>
-            <a href="#contact">Contact</a>
-        </nav>
-    </header>
-    </>
-);
+const Navbar = () => {
+
+    const { headerClass, navbar, logoClass } = useNavStyles();
+
+    return (
+        <>
+        <header id="header" className={headerClass}>
+            <div id="logo" className={logoClass} >
+                Logo
+            </div>
+            <nav className={navbar}>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#skills">Skills</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+        </>
+    );
+}
 
 export default Navbar;
