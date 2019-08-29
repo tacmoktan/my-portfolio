af0bc97324c592b33506bfbe123d2fd939af6e7c
iamtashi <tacmoktan@gmail.com>
2019-08-29 23:56:24 +0545

    Add linear gradient to bg

--- src/section/Project.js
+++ src/section/Project.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const navbarHeight = 57;
 const useProjectStyles = makeStyles(theme => ({
     root:{
-        background:"skyblue",
+        background:"linear-gradient(to bottom, #fddb92 0%, #d1fdff 100%)",
         height:629,     //innerHeight;
         paddingTop:navbarHeight,
         marginTop:-navbarHeight
