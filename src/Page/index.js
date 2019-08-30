3d12a7cbbf2c34a487f3a9e0e2e4765665dab5f5
iamtashi <tacmoktan@gmail.com>
2019-08-30 15:36:40 +0545

    Shift Palette, Add Style variables

--- src/Page/index.js
+++ src/Page/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
-
+import { createMuiTheme } from '@material-ui/core/styles';
 //component
 import Navbar from '../components/Navbar';
 //section
-import Introduction  from '../section/Introduction';
+import Introduction from '../section/Introduction';
 import Project from '../section/Project';
 
+
+const myTheme = createMuiTheme({
+    palette: {
+        primary: {
+            //main: '#AAA139',      //light, dark, contrast will be calculated from palette.primary.main
+            main:'#db7093'
+        },
+        secondary: {
+            main: '#4D2D73'
+        }
+    }
+});
+//style variables
+const styleVar = {
+    myTheme,
+    paddingVar: 20,
+    navbarHeight: 57,
+}
+
 const SinglePage = () => (
-    <>  
-        <Navbar/>
-        <Introduction />
-        <Project />
+    <>
+    <Navbar {...styleVar} />
+    <Introduction {...styleVar} />
+    <Project />
     </>
 );
 
