072df701f2d1ea796777c8a03c042b171dfe26db
iamtashi <tacmoktan@gmail.com>
2019-09-03 20:54:49 +0545

    Fix download CV button

--- src/section/Projects/index.js
+++ src/section/Projects/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core/';
 import { ThemeProvider } from '@material-ui/styles/';
-
 //component
 import ProjectContainer from '../../components/ProjectContainer';
 import projectDescription from './ProjectDescription';
@@ -38,31 +37,35 @@ const Project = ({ navbarHeight, myTheme }) => {
     const { projects, viewMore } = useProjectStyles();
 
     return (
-        <ThemeProvider theme={myTheme}>
-            <section id="projects" className={root}>
-                <div className={title}>
-                    <div>
-                        <i className="fas fa-tasks"></i>
-                        <h1>Projects</h1>
-                        <i className="fas fa-tasks"></i>
+            <ThemeProvider theme={myTheme}>
+
+                <section id="projects" className={root}>
+                    <div className={title}>
+                        <div>
+                            <i className="fas fa-tasks"></i>
+                            <h1>Projects</h1>
+                            <i className="fas fa-tasks"></i>
+                        </div>
+                        <p>Some projects I've worked on</p>
                     </div>
-                    <p>Some projects I've worked on</p>
-                </div>
-                <div className={projects}>
-                    {
-                        projectDescription.map((description, i) =>
-                            <ProjectContainer key={'project_' + i} {...description} myTheme={myTheme} />
-                        )
-                    }
-                </div>
-                <div className={viewMore}>
-                    <h1>VIEW MORE ?</h1>
-                    <Button variant="contained" color="secondary" >
-                        <a href="../../assets/CV/Tashi_Lama_Resume.pdf" download> Download CV </a>
-                    </Button>
-                </div>
-            </section>
-        </ThemeProvider>
+                    <div className={projects}>
+                        {
+                            projectDescription.map((description, i) =>
+                                <ProjectContainer key={'project_' + i} {...description} myTheme={myTheme} />
+                            )
+                        }
+                    </div>
+
+                    <div className={viewMore}>
+                        <h1>VIEW MORE ?</h1>
+                        <Button variant="contained" color="secondary" >
+                            <a href="/files/Tashi_Lama_Resume.pdf" target="_blank" download> Download CV </a>
+                        </Button>
+                    </div>
+
+                </section>
+
+            </ThemeProvider>
     );
 }
 
